de502871e816f3d971d3245cb67c2658832dcfa8
Arttii <artyom.topchyan@live.com>
2024-02-11 19:06:11 +0100

    Update redis.go

--- cache/redis/redis.go
+++ cache/redis/redis.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/bxcodec/httpcache/cache"
+	"github.com/datasapiens/cachier/compression"
 	"github.com/go-redis/redis/v8"
 )
 
@@ -18,23 +19,36 @@ type CacheOptions struct {
 }
 
 type redisCache struct {
-	ctx        context.Context
-	cache      *redis.Client
-	expiryTime time.Duration
+	ctx               context.Context
+	cache             *redis.Client
+	expiryTime        time.Duration
+	compressionEngine *compression.Engine
 }
 
 // NewCache will return the redis cache handler
 func NewCache(ctx context.Context, c *redis.Client, exptime time.Duration) cache.ICacheInteractor {
+	engine, err := compression.NewEngine(compression.ProviderIDZstd, compression.CompressionParams{})
+	if err != nil {
+		panic(err)
+	}
 	return &redisCache{
-		ctx:        ctx,
-		cache:      c,
-		expiryTime: exptime,
+		ctx:               ctx,
+		cache:             c,
+		compressionEngine: engine,
+		expiryTime:        exptime,
 	}
 }
 
 func (i *redisCache) Set(key string, value cache.CachedResponse) (err error) { //nolint
 	valueJSON, _ := json.Marshal(value)
-	set := i.cache.Set(i.ctx, key, string(valueJSON), i.expiryTime*time.Second)
+
+	input, err := i.compressionEngine.Compress(valueJSON)
+	if err != nil {
+
+		return cache.ErrStorageInternal
+	}
+
+	set := i.cache.Set(i.ctx, key, input, i.expiryTime*time.Second)
 	if err := set.Err(); err != nil {
 		fmt.Println(err)
 		return cache.ErrStorageInternal
@@ -43,15 +57,21 @@ func (i *redisCache) Set(key string, value cache.CachedResponse) (err error) { /
 }
 
 func (i *redisCache) Get(key string) (res cache.CachedResponse, err error) {
-	get := i.cache.Do(i.ctx, "get", key)
+	get := i.cache.Get(i.ctx, key)
 	if err = get.Err(); err != nil {
 		if err == redis.Nil {
 			return cache.CachedResponse{}, cache.ErrCacheMissed
 		}
 		return cache.CachedResponse{}, cache.ErrStorageInternal
 	}
-	val := get.Val().(string)
-	err = json.Unmarshal([]byte(val), &res)
+	input, err := i.compressionEngine.Decompress([]byte(get.Val()))
+	if err != nil {
+		// backward compatibility for not compressed entries
+		i.cache.Del(i.ctx, key)
+		return cache.CachedResponse{}, cache.ErrInvalidCachedResponse
+	}
+
+	err = json.Unmarshal(input, &res)
 	if err != nil {
 		return cache.CachedResponse{}, cache.ErrStorageInternal
 	}
